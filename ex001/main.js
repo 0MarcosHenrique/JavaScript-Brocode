13e638763f4543ca890f5792690630f24f598fef
Marcos-Henrique <marcoshenrique.dev27@gmail.com>
2026-04-10 23:18:33 -0300

    First code
    Basics concepts of javascript

---
+++ ex001/main.js
@@ -0,0 +1,9 @@
+// console.log(`Hello`)
+// console.log(`I like AMD!!!`)
+
+
+document.getElementById("myH1").textContent = `hello`;
+document.getElementById("myP").textContent = `i love amd`
+
+// window.alert(`isso é um alerta`)
+// window.alert(`I like AMD`)
