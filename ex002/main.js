5867f0f2656837290f036069acc5b59251882a8f
Marcos-Henrique <marcoshenrique.dev27@gmail.com>
2026-04-11 00:03:05 -0300

    Variables

---
+++ ex002/main.js
@@ -0,0 +1,14 @@
+// Variable = a container of values
+
+//  1. declaration    let x;
+
+// 2. assigment       x = 100;
+
+
+let fullName = "Marcos Henrique";
+let age = "18";
+let isStudent = true;
+
+document.getElementById("p1").textContent = `Your name is ${fullName}`;
+document.getElementById("p2").textContent = `You are ${age} years old`;
+document.getElementById("p3").textContent = `Enrolled: ${isStudent}`;
