850f369ed15bd0901d707480e7fb0dba0d7430bb
Marcos-Henrique <marcoshenrique.dev27@gmail.com>
2026-04-11 22:42:56 -0300

    operators

---
+++ ex003/main.js
@@ -0,0 +1,16 @@
+// arithmetic operators = operands
+//                  loperators (+ - * /)
+//                  ex. 11 = x + 5~
+
+// let students = 30;
+
+/*
+operators precedence
+
+1. parenthesis ()
+2. exponents
+3. multiplication & division & modulo
+4. addition & subtraction
+*/
+
+let result = 1 + 2 * 3 + 4 ** 2;
